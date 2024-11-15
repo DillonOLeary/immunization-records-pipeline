263332efe3710d607994a6fbb6639fdc09d0ee63
Dillon O'Leary <doleary@wisc.edu>
2024-11-15 00:35:32 +0000

    Add a failing test

--- data-pipeline/tests/test_transform.py
+++ data-pipeline/tests/test_transform.py
@@ -5,22 +5,27 @@ Tests for the transformation of the data files
 # pylint: disable=missing-function-docstring
 
 from pandas import DataFrame
-from .fake_data import generate_fake_data
 
-import data_pipeline.transform as transform
+from data_pipeline.transform import transform_data_from_aisr_to_infinite_campus
+
+from .fake_data import generate_fake_data
 
 
 def test_transform_returns_data_frame():
     df_in = DataFrame()
 
-    result = transform.transform_data_from_aisr_to_infinite_campus(df_in)
+    result = transform_data_from_aisr_to_infinite_campus(df_in)
 
     assert isinstance(result, DataFrame)
 
 
 def test_transform_filters_columns():
-    df = generate_fake_data(5)
-    
-    print(df)
+    df_in = generate_fake_data(5)
 
+    result = transform_data_from_aisr_to_infinite_campus(df_in)
+
+    # Define the expected columns in the output dataframe
     expected_columns = ["id_1", "id_2", "vaccine_group_name", "vaccination_date"]
+
+    # Ensure that the output dataframe contains only the expected columns
+    assert list(result.columns) == expected_columns
