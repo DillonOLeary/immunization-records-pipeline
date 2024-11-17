63c77bb520ceb3443f26f9adb639dff552edb803
Dillon O'Leary <doleary@wisc.edu>
2024-11-17 05:48:37 +0000

    Fix main

--- data-pipeline/data_pipeline/main.py
+++ data-pipeline/data_pipeline/main.py
@@ -6,17 +6,18 @@ from pathlib import Path
 
 from data_pipeline.data_flow import run_pipeline
 from data_pipeline.extract import read_from_aisr_csv
+from data_pipeline.load import write_to_infinite_campus_csv
 from data_pipeline.transform import transform_data_from_aisr_to_infinite_campus
 
 if __name__ == "__main__":
+    # pylint: disable=invalid-name
     file_in_path = Path(".") / "tests" / "mock_data" / "mock_aisr_download.csv"
     file_out_path = Path(".") / "output" / "mock_transformed_data.csv"
 
-    # Assuming the pipeline runs and the DataFrame is returned after transformation
-    df_transformed = run_pipeline(
-        data_loader=lambda: read_from_aisr_csv(file_in_path),
-        transformation_function=transform_data_from_aisr_to_infinite_campus,
+    result_message = run_pipeline(
+        extract_function=lambda: read_from_aisr_csv(file_in_path),
+        transform_function=transform_data_from_aisr_to_infinite_campus,
+        load_function=lambda df: write_to_infinite_campus_csv(df, file_out_path),
     )
 
-    # Write the transformed DataFrame to a CSV with a comma separator
-    df_transformed.to_csv(file_out_path, index=False, sep=",")
+    print(result_message)
