b55c6b063379dadcc141fdd8215e1b5b76804d1e
Dillon O'Leary <doleary@wisc.edu>
2024-11-22 06:31:54 +0000

    Simplify code

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -3,6 +3,6 @@
 # Copy the mounted git config to the container file system and make it executable.
 # FIXME I don't think this is working. It's close, but on first container build
 # FIME it fails. There needs to be a little investigation
-cp /tmp/.gitconfig /home/vscode/.gitconfig && chmod u+w /home/vscode/.gitconfig
-# Install the poetry project
-cd /workspaces/immunization-records-pipeline/data-pipeline && poetry install
+# cp /tmp/.gitconfig /home/vscode/.gitconfig && chmod u+w /home/vscode/.gitconfig
+# # Install the poetry project
+# cd /workspaces/immunization-records-pipeline/data-pipeline && poetry install
