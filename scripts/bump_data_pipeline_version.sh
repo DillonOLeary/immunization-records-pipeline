7d64caade2b4952251211aa7d8bcc34f23d7f814
Dillon O'Leary <doleary@wisc.edu>
2024-12-02 04:27:33 +0000

    Modify version bump

--- scripts/bump_data_pipeline_version.sh
+++ scripts/bump_data_pipeline_version.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-# THIS IS FOR REFERENCE ONLY
 
-# This will bump the poetry version and add a git tag. Really useful for CD
-# Assuming this is run from the data-pipeline directory
+# Ensure the script exits on errors
+set -e
+
+# Ensure this script is run from the 'data-pipeline' directory
 
 # Run Poetry version bump (e.g., patch, minor, major)
-# poetry version $1
+poetry version $1
 
 # Get the new version from pyproject.toml
-# NEW_VERSION=$(poetry version -s)
+NEW_VERSION=$(poetry version -s)
 
-# The code needs to be pushed here or else pypi will not
-# get the newest package version for the release
+# Commit the updated pyproject.toml (and lock file if changed)
+git reset
+git add pyproject.toml poetry.lock
+git commit -m "Bump version to $NEW_VERSION"
+
+# Push the updated code to the remote repository
+git push origin main  # or your current branch
 
 # Create a Git tag with the new version
-# git tag "v$NEW_VERSION"
+git tag "v$NEW_VERSION"
 
 # Push the Git tag to the remote repository
-# git push origin "v$NEW_VERSION"
+git push origin "v$NEW_VERSION"
