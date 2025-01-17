ca59c687944bc8ac946fb8c3ea7b82313a238f80
Dillon O'Leary <doleary@wisc.edu>
2025-01-17 04:56:54 +0000

    Selenium setup

--- data-pipeline/data_pipeline/query.py
+++ data-pipeline/data_pipeline/query.py
@@ -26,4 +26,15 @@ def login(web_driver: WebDriver, username: str, password: str) -> AISRResponse:
     """
     logger.info("Logging into MIIC")
 
+    # username_input = web_driver.find_element(By.ID, "username")
+    # username_input.clear()
+    # username_input.send_keys(username)
+
+    # password_input = web_driver.find_element(By.ID, "password")
+    # password_input.clear()
+    # password_input.send_keys(password)
+
+    # login_button = web_driver.find_element(By.ID, "kc-login")
+    # login_button.click()
+
     return AISRResponse(is_successful=False, message="Failed")
