bef9551988b9f711972f3f8b9602c06169894a9c
Dillon O'Leary <dillon.oleary@icloud.com>
2025-03-18 06:33:34 +0000

    Create UV projects

---
+++ minnesota-immunization-cli/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from minnesota-immunization-cli!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ minnesota-immunization-cloud/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from minnesota-immunization-cloud!")
+
+
+if __name__ == "__main__":
+    main()
